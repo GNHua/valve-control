1ccbb5b789e68c803b17a730584a5165eb9ba5b8
GNHua <gonghuabupt@gmail.com>
2018-08-01 13:02:19 -0600

    add update function

--- app/main.py
+++ app/main.py
@@ -2,14 +2,21 @@ import os
 import sys
 from PyQt5 import QtWidgets, uic
 import serial.tools.list_ports
+import urllib.request
+import glob
+import shutil
+import zipfile
+import webbrowser
 
+from app import __version__
 from app.ui.connect import UsbPortsTableDialog
 from app.ui.flow_layout import FlowLayout
 from app.ui.built_in_program_dialog import ToggleValveDialog, FivePhasePumpDialog
 from app.valve_control import ValveControlDevice, ToggleValveCycle, FivePhasePumpCycle
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-Ui_MainWindow, QMainWindow = uic.loadUiType(os.path.join(ROOT_DIR, 'ui', 'main.ui'))
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(APP_DIR, '..'))
+Ui_MainWindow, QMainWindow = uic.loadUiType(os.path.join(APP_DIR, 'ui', 'main.ui'))
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -18,7 +25,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.flowLayoutValveControl = FlowLayout(parent=self.frameValveControl)
-        with open(os.path.join(ROOT_DIR, 'ui', 'style_sheet.txt'), 'r') as f:
+        with open(os.path.join(APP_DIR, 'ui', 'style_sheet.txt'), 'r') as f:
             styleSheet = f.read()
         self.setStyleSheet(styleSheet)
 
@@ -31,7 +38,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButtonStop.clicked.connect(self.stop)
         self.pushButtonLoadBuiltIn.clicked.connect(self.loadBuiltInProgram)
         self.actionOpen.triggered.connect(self.uploadProgram)
-        self.actionClose.triggered.connect(self.closeEvent)
+        self.actionClose.triggered.connect(self.close)
         self.actionRestart_Device.triggered.connect(self.restartDevice)
         self.actionTurn_Off_All.triggered.connect(self.clear)
         self.actionStart.triggered.connect(self.start)
@@ -39,8 +46,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.actionGroupSR.triggered.connect(self.changeSR)
         self.actionReset_Input.triggered.connect(self.serialResetInput)
         self.actionReset_Input.triggered.connect(self.serialResetOutput)
+        self.actionDocumentation.triggered.connect(self.openGithubRepo)
+        self.actionUpdate.triggered.connect(self.update)
 
-        self.dir = ROOT_DIR
+        self.dir = APP_DIR
 
     def getUsbPort(self):
         ports = serial.tools.list_ports.comports()
@@ -162,14 +171,66 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def serialResetOutput(self):
         self.device.reset_output_buffer()
 
+    def openGithubRepo(self):
+        webbrowser.open_new('https://github.com/GNHua/valve-control')
+
+    def update(self):
+        currentVersion = __version__
+
+        # check the latest version number
+        link = 'https://raw.githubusercontent.com/GNHua/valve-control/master/app/__init__.py'
+        with urllib.request.urlopen(link) as f:
+            _code = f.read()
+            exec(_code, globals())
+            latestVersion = __version__
+
+        if currentVersion == latestVersion:
+            QtWidgets.QMessageBox.information(
+                self, 'Info',
+                'This is the latest version.',
+                QtWidgets.QMessageBox.Ok)
+            return
+
+        # clean root directory
+        for f in glob.glob(os.path.join(ROOT_DIR, '*')):
+            if os.path.isdir(f):
+                shutil.rmtree(f)
+            else:
+                os.remove(f)
+
+        # download the new version of code
+        dwn_link = 'https://github.com/GNHua/valve-control/archive/master.zip'
+        filename = os.path.join(ROOT_DIR, 'update.zip')
+        urllib.request.urlretrieve(dwn_link, filename)
+
+        # unzip downloaded file and extract them to root directory
+        with zipfile.ZipFile(filename, 'r') as zf:
+            folder = zf.namelist()[0]
+            zf.extractall(path=ROOT_DIR)
+            for f in glob.glob(os.path.join(ROOT_DIR, folder, '*')):
+                if os.path.isdir(f):
+                    shutil.copytree(f, os.path.join(ROOT_DIR, os.path.basename(f)))
+                else:
+                    shutil.copy(f, ROOT_DIR)
+            # remove downloaded files
+            shutil.rmtree(os.path.join(ROOT_DIR, folder))
+            os.remove(filename)
+
+        QtWidgets.QMessageBox.information(
+            self, 'Info',
+            'The program will be closed.\n'
+            'Restart it to run the latest vesion.',
+            QtWidgets.QMessageBox.Ok)
+        self.close()
+
     def show(self):
         super().show()
         self.getUsbPort()
 
     def closeEvent(self, event):
+        print('closing')
         self.clear()
         self.device.close()
-        self.close()
         event.accept()
 
 
